6a0fe0a0fa870c1810a9121e1e4092f12917e534
Jose Trezza <josetrezza@gmail.com>
2015-05-03 07:42:38 -0500

    Initial migrations created

--- database/migrations/2015_05_03_122210_create_post_states_table.php
+++ database/migrations/2015_05_03_122210_create_post_states_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreatePostStatesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,10 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('post_states', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('username')->unique();
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+			$table->string('description');
 			$table->timestamps();
 		});
 	}
@@ -31,7 +27,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('post_states');
 	}
 
 }
---
+++ database/migrations/2015_05_03_122415_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('slug')->unique();
+			$table->string('title');
+			$table->integer('parser_id')->references('id')->on('parsers');
+			$table->longText('body');
+			$table->integer('state_id')->references('id')->on('states');
+			$table->integer('user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
